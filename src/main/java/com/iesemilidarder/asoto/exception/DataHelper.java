e280acd760d3245ab2acff9af61b988d470ed994
Maribelm <mmp02@iesemilidarder.com>
2018-11-08 16:02:32 +0100

    h

--- src/main/java/com/iesemilidarder/asoto/exception/DataHelper.java
+++ src/main/java/com/iesemilidarder/asoto/exception/DataHelper.java
@@ -14,33 +14,30 @@ public class DataHelper {
 
     public DataHelper() {
 
-        public static void main (String[]args){
-            String[] actividades = new String[]{"Montañismo,", "Monumentos,", "Esquí,", "Visitas a pueblos,", "Comer", "Hotel"};
-            String[] tiposactividades = new String[]{"Cultura,", "Gatronomía,", "Senderismo", "Hotel"};
+        String[] actividades = new String[]{"Montañismo,", "Monumentos,", "Esquí,", "Visitas a pueblos,", "Comer", "Hotel"};
+        String[] tiposactividades = new String[]{"Cultura,", "Gatronomía,", "Senderismo", "Hotel"};
 
-            String[] excursiones = new String[]{"GR211", "GR122", "GR666", "GR122"};
-            for (int x = 0; x < excursiones.length; x++) {
-                Excursion a = new Excursion();
-                a.setName(excursiones[x]);
-                products.add(a);
-            }
-            for (String excursion : excursiones) {
-                Excursion a = new Excursion();
-                a.setName(excursion);
-                a.setPais(new Pais("España"));
-                products.add(a);
-            }
+        String[] excursiones = new String[]{"GR211", "GR122", "GR666", "GR122"};
+        for (int x = 0; x < excursiones.length; x++) {
+            Excursion a = new Excursion();
+            a.setName(excursiones[x]);
+            products.add(a);
+        }
+        for (String excursion : excursiones) {
+            Excursion a = new Excursion();
+            a.setName(excursion);
+            a.setPais(new Pais("España"));
+            products.add(a);
+        }
 
 
-            for (int x = 0; x < actividades.length; x++) {
-                System.out.println(actividades[x]);
-            }
-
+        for (int x = 0; x < actividades.length; x++) {
+            System.out.println(actividades[x]);
         }
 
     }
 
-    public static List<Product> getData() {
+    public static void getData() {
     }
 
     public static void insert(Vuelo aux) {
